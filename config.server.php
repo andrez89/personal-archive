a9eebc7515adf2f5e3af6b1cb1701bfbe63dd59e
Andrea Zaccara <andrea.zaccara89@gmail.com>
2020-11-09 13:39:07 +0100

    fix nuovo server

--- config.server.php
+++ config.server.php
@@ -2,10 +2,10 @@
 
 return [
     'database' => [
-        'name' => 'id11900121_andrez',
-        'username' => 'id11900121_andrez',
-        'password' => 'Jkv@dS74b!jkEfs',
-        'connection' => 'mysql:host=localhost',
+        'name' => 'resources',
+        'username' => 'resources',
+        'password' => 'reS298re',
+        'connection' => 'mysql:host=za48251-001.dbaas.ovh.net:35947',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
